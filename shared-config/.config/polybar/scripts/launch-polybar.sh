17d1ca379f58e540f7be8465e7ebc57d7e02a3f8
yannikkellerde <yannik@kelnet.de>
2023-04-30 02:12:17 +0200

    final

--- shared-config/.config/polybar/scripts/launch-polybar.sh
+++ shared-config/.config/polybar/scripts/launch-polybar.sh
@@ -7,7 +7,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar
-polybar example &
+polybar main &
+polybar second &
 
 sleep 1
 xdo lower -N Polybar
